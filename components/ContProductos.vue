387828a3fe9d8d36921d4b59f621b893216b8a7c
Frank <frankortizm849@gmail.com>
2021-02-10 18:28:32 -0600

    finalizado esqueleto, salto a otra rama child para probar slider

---
+++ components/ContProductos.vue
@@ -0,0 +1,16 @@
+<template >
+    <div class="contProduct">
+    <producto/>    
+    <producto/>    
+    </div>
+</template>
+<script>
+import Producto from './Producto.vue'
+export default {
+  components: { Producto },
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ components/Producto.vue
@@ -0,0 +1,18 @@
+<template >
+    <div class="productEspecific">
+        <img class="productEspecific_img" src="https://dam.vanidades.com/wp-content/uploads/2020/04/%C2%A1Perritos-podr%C3%ADan-detectar-coronavirus.jpg" alt="">
+        <div class="productEspecific_info">
+            <p class="productEspecific_info_text">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <p class="productEspecific_info_price">$4.99</p>
+            <div class="productEspecific_info_background"></div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
