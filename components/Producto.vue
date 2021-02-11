92e9574316cca38cd66bbf1a9da4b91f95d91433
Frank <frankortizm849@gmail.com>
2021-02-11 16:20:22 -0600

    body finalizado para demos modificaciones, conexion a base de datos lista junto con store config

--- components/Producto.vue
+++ components/Producto.vue
@@ -2,15 +2,18 @@
     <div class="productEspecific">
         <img class="productEspecific_img" src="https://dam.vanidades.com/wp-content/uploads/2020/04/%C2%A1Perritos-podr%C3%ADan-detectar-coronavirus.jpg" alt="">
         <div class="productEspecific_info">
-            <p class="productEspecific_info_text">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-            <p class="productEspecific_info_price">$4.99</p>
+            <p class="productEspecific_info_text">{{nombre}}</p>
+            <p class="productEspecific_info_price">{{precio}}</p>
             <div class="productEspecific_info_background"></div>
         </div>
     </div>
 </template>
 <script>
 export default {
-    
+     props: {
+    nombre: String,
+    precio: Number
+  },
 }
 </script>
 <style lang="">
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="container">
     <div class="index_baner">
-      <img class="index_baner_img" src="https://www.yaconic.com/wp-content/uploads/2020/02/kitten-2288404_1920-1024x683.jpg" alt="">
+      <img
+        class="index_baner_img"
+        src="https://www.yaconic.com/wp-content/uploads/2020/02/kitten-2288404_1920-1024x683.jpg"
+        alt=""
+      />
     </div>
-    <div class="index_slider">
-      slider
+
+    <div style="height: 150px; overflow: hidden; width: 100%">
+      <svg
+        viewBox="0 0 500 150"
+        preserveAspectRatio="none"
+        style="height: 100%; width: 100%"
+      >
+        <path
+          d="M-15.12,123.65 C377.65,163.13 495.60,100.95 511.96,147.32 L558.80,194.70 L0.00,150.00 Z"
+          style="stroke: none; fill: #8b48e6"
+        ></path>
+      </svg>
     </div>
+
+    <div class="index_slider">slider</div>
+
+    <div style="height: 100px; overflow: hidden; width: 100%;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-30.92,97.99 C329.11,-105.29 437.47,218.39 505.19,20.02 L500.00,0.00 L0.00,0.00 Z" style="stroke: none; fill: #8B48E6; height: 200%;"></path></svg></div>
+
     <div class="index_categorys">
       <h2 class="index_categorys_title">CATEGORIAS</h2>
       <div class="indez_category_content">
@@ -20,23 +39,32 @@
       </div>
     </div>
 
-
     <div class="index_maylike">
       <h2 class="index_maylike_title">Puede que te guste</h2>
-      <cont-productos/>
-      <p class="index_seeMore">ver mas</p>
+      <cont-productos />
+      <p @click="seeMore" class="index_seeMore">ver mas</p>
     </div>
-
-    
   </div>
 </template>
 
 <script>
-import ContProductos from '../components/ContProductos.vue'
+import ContProductos from "../components/ContProductos.vue";
+import { mapMutations } from "vuex";
+
 export default {
   components: { ContProductos },
-  
-}
+  data() {
+    return {};
+  },
+  methods: {
+    ...mapMutations({
+      seeMore: "products/moreProds",
+    }),
+  },
+  mounted() {
+    //this.getCommetns();
+  },
+};
 </script>
 
 
