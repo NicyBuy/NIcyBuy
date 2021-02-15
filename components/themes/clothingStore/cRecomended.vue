e10c38c4d4baa3e7ae25a5d0ad4d19648c2c0ff1
Edgardo <edgardo.avalencia@gmail.com>
2021-02-14 22:21:22 -0600

    se termino la pagina de producto especifico

--- components/themes/clothingStore/cRecomended.vue
+++ components/themes/clothingStore/cRecomended.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="contProducts">
-    <div class="Products">
+  <div class="cRecomended">
+     <div class="Recomended">
+
+    <div class="Title">
+      Recomendados
+    </div>
+
+    <div class="white-space"></div>
+    <div class="Recomended__Products">
 
       <cProduct :img="img1" name="Traje amarillo y azul para mujer" price=10.99 />
 
@@ -17,14 +24,19 @@
     </div>
 
     <div class="ver-mas"><button>ver mas</button></div>
+    <div class="white-space"></div>
   </div>
+  <c-footer/>
+  </div>
+ 
 </template>
 
 <script>
 import cProduct from './cProduct.vue'
 import img1 from '../../../assets/Stores/ThriftyShop/products/1.jpg'
+import CFooter from './cFooter.vue'
 export default {
-  components: { cProduct },
+  components: { cProduct, CFooter },
 
   data(){
     return{
@@ -37,3 +49,4 @@ export default {
 </script>
 
 <style lang = "scss" src = "../../../scss/main.scss"></style>
+
--- layouts/themes/clothingStore/productView.vue
+++ layouts/themes/clothingStore/productView.vue
@@ -1,35 +1,42 @@
 <template>
 <div class="productView">
-     <c-nav/>
-      <div class="nav-space"></div>
-     <div class="productView__contImg">
-         <img :src="product" alt="">
-     </div>
+  <c-nav />
+  <div class="nav-space"></div>
+  <div class="productView__contImg">
+    <img :src="product" alt="">
+  </div>
 
-     <main>
-         <div class="contInfo">
-             <div class="product__Name">Camisa color azulito para hombre talla M</div>
-             <div class="product__Description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque possimus modi voluptate eaque. Ipsa enim dolores doloribus accusantium dolore iure atque qui ad placeat sit.</div>
-             <div class="size">Talla: <span>M</span></div>
+  <main>
+    <div class="contInfo">
+      <div class="product__Name">
+        Camisa color azulito para hombre talla M
+      </div>
+      <div class="product__Description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque possimus modi
+        voluptate eaque. Ipsa enim dolores doloribus accusantium dolore iure atque qui ad placeat sit.
+      </div>
+      <div class="size">Talla: <span>M</span></div>
 
-             <div class="contPreview">
-                 <div class="preview"></div>
-                 <div class="preview"></div>
-                 <div class="preview"></div>
-                 <div class="preview"></div>
-             </div>
+      <div class="contPreview">
+        <div class="preview"></div>
+        <div class="preview"></div>
+        <div class="preview"></div>
+        <div class="preview"></div>
+      </div>
 
-             <div class="contPrice">
-                 <div class="price">
-                     $10.99
-                 </div>
+      <div class="contPrice">
+        <div class="price">
+          $10.99
+        </div>
 
-                 <button>
-                     COMPRAR
-                 </button>
-             </div>
-         </div>
-     </main>
+        <button>
+          COMPRAR
+        </button>
+      </div>
+    </div>
+  </main>
+
+  <c-recomended/>
 
 </div>
 </template>
@@ -37,8 +44,9 @@
 <script>
 import cNav from '../../../components/themes/clothingStore/cNav.vue'
 import product from '../../../assets/Stores/ThriftyShop/products/2.jpg';
+import CRecomended from '../../../components/themes/clothingStore/cRecomended.vue';
 export default {
-  components: { cNav, },
+  components: { cNav, CRecomended, },
 
   data(){
       return{
