fa676ebeb4d596ea40c6685237167ce0fe53a5d9
Edgardo <edgardo.avalencia@gmail.com>
2021-02-23 09:29:22 -0600

    se agrego la imagen de cover

--- layouts/themes/foodStore/home.vue
+++ layouts/themes/foodStore/home.vue
@@ -1,17 +1,30 @@
 <template>
 <div class="home">
     <cNav/>
-     
+     <div class="foodStore__Cover">
+         <img :src="Cover" alt="">
+     </div>
 </div>
 
  
 </template>
 
 <script>
+//components
 import cNav from '../../../components/themes/foodStore/cNav.vue'
+
+//img
+import Cover from '../../../assets/Stores/foodStore/img/Logo.jpg'
+
 export default {
     components: {
         cNav
+    },
+
+    data(){
+        return{
+            Cover: Cover
+        }
     }
 }
 </script>
