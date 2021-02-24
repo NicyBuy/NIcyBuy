49418f1889445709517bdef0f1b6262ef218c44b
Edgardo <edgardo.avalencia@gmail.com>
2021-02-23 21:58:09 -0600

    se empezo la pagina de home de foodStore

--- layouts/themes/foodStore/home.vue
+++ layouts/themes/foodStore/home.vue
@@ -1,9 +1,40 @@
 <template>
 <div class="home">
-    <cNav/>
-     <div class="foodStore__Cover">
-         <img :src="Cover" alt="">
-     </div>
+  <cNav />
+  <div class="foodStore__main">
+    <div class="foodStore__Cover">
+      <img :src="Cover" alt="">
+    </div>
+
+    <div class="foodStore__cont-featured">
+        <div class="--white-space"></div>
+        <div class="foodStore__featured">
+            <div class="foodStore__title">
+                MEJORES RECETAS
+            </div>
+
+            <div class="foodStore__featured-products">
+
+            </div>
+        </div>
+    </div>
+    <div class="--whte-space"></div>
+    <div class="foodStore__cont-categories">
+        <div class="foodStore__title">
+                CATEGORIES
+            </div>
+        <div class="white-space"></div>
+
+        <div class="foodStore__categories">
+            <button>CATEGORY</button>
+            <button>CATEGORY</button>
+             <button>CATEGORY</button>
+            <button>CATEGORY</button>
+        </div>
+    </div>
+
+
+  </div>
 </div>
 
  
@@ -12,7 +43,6 @@
 <script>
 //components
 import cNav from '../../../themes/FoodStore/components/cNav';
-
 //img
 import Cover from '../../../themes/FoodStore/assets/img/Logo.jpg';
 
