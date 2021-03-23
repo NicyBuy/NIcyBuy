ae2f3c8cd0f7596f273cfce5034a677468627fbe
Edgardo <edgardo.avalencia@gmail.com>
2021-03-22 22:30:25 -0600

    Se empezo la visualizacion de producto

--- layouts/themes/clothingStore/productView.vue
+++ layouts/themes/clothingStore/productView.vue
@@ -2,6 +2,15 @@
 <div class="productView">
   <c-nav />
 
+  <div class="preview-off">
+    <div class="cont-x">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
+    </div>
+    <div class="preview-off__contImg">
+      <img :src="product" alt="">
+    </div>
+  </div>
+
   <div class="productView__contImg">
     <img :src="product" alt="">
   </div>
@@ -20,9 +29,13 @@
       <div class="size">Talla: <span>M</span></div>
 
       <div class="contPreview">
-        <div class="preview"></div>
-        
+        <div class="preview">
+          <img :src="product" alt="">
+        </div>
+
+
       </div>
+
 
       <div class="contPrice">
         <div class="price">
@@ -37,7 +50,6 @@
   </main>
 
   <c-recomended />
-
 </div>
 </template>
 
@@ -56,8 +68,30 @@ export default {
     return {
       product: product
     }
+  },
+
+  mounted(){
+    const previewToggle =() =>{
+
+    
+    const preview = document.querySelector(".preview");
+    const previewOff = document.querySelector(".preview-off");
+    const exit = document.querySelector(".cont-x");
+
+    preview.addEventListener("click", ()=>{
+      console.log("se activo preview");
+      previewOff.classList.add("preview-on");
+    })
+
+    exit.addEventListener("click", ()=>{
+      console.log("se activo preview");
+      previewOff.classList.remove("preview-on");
+    })
+
   }
 
+  previewToggle();
+}
 }
 </script>
 
