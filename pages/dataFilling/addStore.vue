1bc582958616afd15251976dde78e2031be5bc80
edgardo <edgardo.avalencia@gmail.com>
2021-05-08 16:46:44 -0600

    Se hizo la pagina de addStore

---
+++ pages/dataFilling/addStore.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="dataFilling__addStore">
+    <cNav />
+    <div class="dataFilling__addStore-main">
+      <input type="tel" class="dataFilling__addStore-name" placeholder="Nombre de la Tienda" name="addStoreName"
+        id="addStoreName">
+      <div class="dataFilling__addStore-owner">
+        <span>De:</span> <input type="text" class="dataFilling__addStore-owner-input" placeholder="Propietario" name="addStoreOwner" id="addStoreOwner">
+      </div>
+
+
+      <button class="dataFilling__addStore-trigger1">Productos</button>
+      <button class="dataFilling__addStore-trigger1">Productos</button>
+      <button class="dataFilling__addStore-trigger1">Productos</button>
+    </div>
+  </div>
+
+</template>
+
+<script>
+//componets
+import cNav from "../../components/dataFilling/cNav.vue";
+
+//img
+import Logo from "../../assets/dataFillingAssets/koos PNG.png"
+export default {
+ layout: "dataFillingLayout",
+
+components: {
+  cNav,
+},
+
+data(){
+  return {
+    Logo,
+  }
+
+}
+}
+</script>
+
+
+<style lang="scss" src="../../scss/main.scss"></style>
