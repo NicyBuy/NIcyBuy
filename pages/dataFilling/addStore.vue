90cc12e04347f9d6a8e39626f914d9af4df773d4
edgardo <edgardo.avalencia@gmail.com>
2021-05-11 20:17:48 -0600

    Se agrego el display a la pagina de addStore

--- pages/dataFilling/addStore.vue
+++ pages/dataFilling/addStore.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="dataFilling__addStore">
     <cNav />
+    <!-- Main Container -->
     <div class="dataFilling__addStore-main">
+      <!-- Name of the Store -->
       <input type="tel" class="dataFilling__addStore-name" placeholder="Nombre de la Tienda" name="addStoreName"
         id="addStoreName">
+      <!-- Name of the owner of the Store -->
       <div class="dataFilling__addStore-owner">
-        <span>De:</span> <input type="text" class="dataFilling__addStore-owner-input" placeholder="Propietario" name="addStoreOwner" id="addStoreOwner">
+        <span>De:</span> <input type="text" class="dataFilling__addStore-owner-input" placeholder="Propietario"
+          name="addStoreOwner" id="addStoreOwner">
       </div>
 
+      <button @click="display =! display" class="dataFilling__addStore-trigger1">Productos</button>
 
-      <button class="dataFilling__addStore-trigger1">Productos</button>
-      <button class="dataFilling__addStore-trigger1">Productos</button>
-      <button class="dataFilling__addStore-trigger1">Productos</button>
+      <div :class="Show" class="dataFilling__addStore-contTrigger2">
+        <button class="dataFilling__addStore-trigger2">Agregar</button>
+
+        <button class="dataFilling__addStore-trigger2">Editar</button>
+      </div>
+
+      <button class="dataFilling__addStore-trigger1">Informacion</button>
+
+      <button class="dataFilling__addStore-trigger1">Contacto</button>
     </div>
   </div>
 
@@ -32,9 +43,20 @@ components: {
 
 data(){
   return {
+    //img
     Logo,
-  }
 
+    //vars
+    display: false,
+  }
+},
+
+computed: {
+  Show(){
+    return{
+      display: this.display
+    }
+  }
 }
 }
 </script>
