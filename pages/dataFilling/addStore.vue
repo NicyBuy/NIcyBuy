8785a1d40978a55861007c8726976eb0e420d871
edgardo <edgardo.avalencia@gmail.com>
2021-05-17 18:12:52 -0600

    Se completo la parte de informacion

--- pages/dataFilling/addStore.vue
+++ pages/dataFilling/addStore.vue
@@ -1,47 +1,78 @@
 <template>
   <div class="dataFilling__addStore">
+    <!-- Add Product Container ---------------------------------------------- -->
     <div :class="ShowAgregar" class="dataFilling__addStore-contAddProduct">
       <div class="dataFilling__addStore-addProduct">
         <button @click="display2 =! display2">x</button>
 
         <div class="dataFilling__addProduct-main">
-          <input type="text" class="dataFilling__addProduct-title" name="addProductTitle" id="addProductTitle" maxlength="77"
-              placeholder="Titulo">
+          <input type="text" class="dataFilling__addProduct-title" name="addProductTitle" id="addProductTitle"
+            maxlength="77" placeholder="Titulo">
+
           <div class="dataFilling__addProduct-main-data">
             <textarea name="addProductDescription" id="addProductDescription" cols="30" rows="10" maxlength="1200"
               placeholder="Descripcion"></textarea>
 
-              <div class="dataFilling__addProduct-contImageUpload">
-                <label for="addProductImage1">Imagen Principal</label>
-                <input class="dataFilling__addProduct-ImageUpload1"  type="file" name="addProductImage1" id="addProductImage1">
-              </div>
+            <div class="dataFilling__addProduct-contImageUpload">
+              <input class="dataFilling__addProduct-ImageUpload1" type="file" name="addProductImage1"
+                id="addProductImage1">
+            </div>
+
+
           </div>
         </div>
+        <input class="dataFilling__addStore-addProduct-summit" type="submit" @click="display2 =! display2"
+          value="aceptar">
       </div>
     </div>
+    <!-- ------------------------------------------------------------------------------------------------------------------------- -->
+
+
+
     <cNav />
     <!-- Main Container -->
     <div class="dataFilling__addStore-main">
+
       <!-- Name of the Store -->
       <input type="tel" class="dataFilling__addStore-name" placeholder="Nombre de la Tienda" name="addStoreName"
         id="addStoreName">
+
       <!-- Name of the owner of the Store -->
       <div class="dataFilling__addStore-owner">
         <span>De:</span> <input type="text" class="dataFilling__addStore-owner-input" placeholder="Propietario"
           name="addStoreOwner" id="addStoreOwner">
       </div>
 
+      <!-- Productos----------------------------------------------------------------------------- -->
       <button @click="display1 =! display1" class="dataFilling__addStore-trigger1">Productos</button>
 
-      <div :class="ShowProductos" class="dataFilling__addStore-contTrigger2">
+      <div :class="ShowProductosOptions" class="dataFilling__addStore-contTrigger2">
         <button class="dataFilling__addStore-trigger2" @click="display2 = ! display2">Agregar</button>
 
-        <button class="dataFilling__addStore-trigger2">Editar</button>
+        <button @click="display3 =! display3" class="dataFilling__addStore-trigger2">Editar</button>
+
+        <div :class="ShowEditar" class="dataFilling__addStore-contEditProductos"></div>
       </div>
+      <!-- ----------------------------------------------------------------------------------------------- -->
 
-      <button class="dataFilling__addStore-trigger1">Informacion</button>
+      <!-- Informacion ------------------------------------------------------------------------------------ -->
+      <button @click="display4 =! display4" class="dataFilling__addStore-trigger1">Informacion</button>
 
-      <button class="dataFilling__addStore-trigger1">Contacto</button>
+      <div :class="ShowInformacion" class="dataFilling__addStore-contInformacion">
+        <h2>Que somos?</h2>
+        <textarea name="dataFillingQueSomos" id="dataFillingQueSomos" cols="30" rows="10"></textarea>
+        <textarea name="dataFillingQueSomos" id="dataFillingQueSomos" cols="30" rows="10"></textarea>
+        <input type="file" name="imgInformacion" id="imgInformacion">
+      </div>
+      <!-- ------------------------------------------------------------------------------------------------- -->
+
+      <!-- Contacto ---------------------------------------------------------------------------------------- -->
+      <button @click="display5 =! display5" class="dataFilling__addStore-trigger1">Contacto</button>
+
+      <div :class="ShowContacto" class="dataFilling__addStore-contContacto">
+        <h2>Contacto</h2>
+      </div>
+      <!-- ------------------------------------------------------------------------------------------------- -->
     </div>
   </div>
 </template>
@@ -67,12 +98,15 @@ data(){
     //vars
     display1: false,
     display2: false,
+    display3: false,
+    display4: false,
+    display5: false,
     
   }
 },
 
 computed: {
-  ShowProductos(){
+  ShowProductosOptions(){
     return{
       display1: this.display1
     }
@@ -81,6 +115,24 @@ computed: {
   ShowAgregar(){
     return{
       display2: this.display2
+    }
+  },
+
+  ShowEditar(){
+    return{
+      display3: this.display3
+    }
+  },
+
+  ShowInformacion(){
+    return{
+      display4: this.display4
+    }
+  },
+
+  ShowContacto(){
+    return{
+      display5: this.display5
     }
   }
 }
